a698d10e670a4e13ab7f8faf2024a2f4c2178c0b
Chris Sayles <chrissayles@yahoo.com>
2017-05-12 10:59:44 -0500

    playerCreate route added

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var playerController = require('../controllers/playerController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/api/:name', function(req, res, next) {
-
+router.post('/api/playerCreate', function(req,res,next){
+  let data = req.body;
+  playerController.create(req.body, function(err,res){
+    if(err){
+      res.json({
+        confirmation: 'fail',
+        message: err
+      })
+      return;
+    }
+    res.json({
+      confirmation: 'success',
+      message: res
+    });
+  });
 });
 
 
