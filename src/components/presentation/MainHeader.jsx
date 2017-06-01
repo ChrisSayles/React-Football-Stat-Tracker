5450dc3a288f886ea5d963f2d87899475084ec46
gitbchang <brian.h.chang1@gmail.com>
2017-06-01 09:34:58 -0500

    Link changes from a tags

--- src/components/presentation/MainHeader.jsx
+++ src/components/presentation/MainHeader.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class MainHeader extends Component {
   render() {
@@ -15,35 +16,17 @@ class MainHeader extends Component {
             aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <a className="navbar-brand" href="#">React Football!</a>
+          <Link to="/" className="navbar-brand">React Football</Link>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
             <ul className="navbar-nav navbar-right">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home
-                  <span className="sr-only">(current)</span>
-                </a>
+                <Link to="/" className="nav-link">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Data Visuals</a>
+                <Link to="/data" className="nav-link">Data Visuals</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Favorite Players</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="http://example.com"
-                  id="navbarDropdownMenuLink"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false">
-                  Dropdown link
-                </a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a className="dropdown-item" href="#">Action</a>
-                  <a className="dropdown-item" href="#">Another action</a>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </div>
+                <Link to="/favorite" className="nav-link" >Favorite Players</Link>
               </li>
             </ul>
           </div>
--- src/components/presentation/TopTeamPlayers.jsx
+++ src/components/presentation/TopTeamPlayers.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import BarChart from 'britecharts/dist/umd/bar.min';
 import colors from 'britecharts/src/charts/helpers/colors';
 import miniTooltip from 'britecharts/src/charts/mini-tooltip';
-const d3Selection = require('d3-selection');
+import d3Selection from 'd3-selection';
 
 
 class TopTeamPlayers extends Component {
